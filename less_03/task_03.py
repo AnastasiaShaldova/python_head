517d35c71d1bd33755b28338ba9e72e27ec7986e
Any4rd <Any4rd@gmail.com>
2023-08-27 15:28:05 +0300

    [+] решение задачи №3

--- less_03/task_03.py
+++ less_03/task_03.py
@@ -27,3 +27,28 @@
 #
 # k = 'ноутбук'
 # 12
+
+dictionary = {
+    1: ['А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т', 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R'],
+    2: ['Д', 'К', 'Л', 'М', 'П', 'У', 'D', 'G'],
+    3: ['Б', 'Г', 'Ё', 'Ь', 'Я', 'B', 'C', 'M', 'P'],
+    4: ['Й', 'Ы', 'F', 'H', 'V', 'W', 'Y'],
+    5: ['Ж', 'З', 'Х', 'Ц', 'Ч', 'K'],
+    8: ['Ш', 'Э', 'Ю', 'J', 'X'],
+    10: ['Ф', 'Щ', 'Ъ', 'Q', 'Z']
+}
+
+
+def word_value(word: str):
+    count = 0
+    res_word = [i for i in word.upper()]
+    for j in res_word:
+        for k, v in dictionary.items():
+            for i in v:
+                if i == j:
+                    count += k
+                    break
+    return count
+
+
+print(word_value('lime'))
