ed5b7607425aa7d74185823c654c7195370c24d4
Any4rd <Any4rd@gmail.com>
2023-09-07 09:18:46 +0300

    [-] неиспользуемый импорт

--- less_06/task_02.py
+++ less_06/task_02.py
@@ -2,7 +2,7 @@
 Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
 (т.е. не меньше заданного минимума и не больше заданного максимума)
 """
-from random import random, randint
+from random import randint
 
 
 list_generator = list(randint(-10, 10) for i in range(20))
